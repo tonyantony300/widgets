2b4418d660ace351c814077d4bacf9f0cd42bb36
TONY ANTONY <tonyantony300@gmail.com>
2022-06-06 19:47:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+
+
 
 const items = [
 { title: 'what is React',
@@ -15,10 +18,35 @@ const items = [
 
 ]
 
+const options = [
+    {
+    label:'The color Red',
+    value:'Red'
+    },
+    {
+    label:'The color Green',
+    value:'Green'
+    },
+    {
+    label:'A shade of Blue',
+    value:'Blue'
+    },
+]
+
+
 
 export default (() => {
+
+const [selected, setSelected] = useState(options[0]);
+
+
 return (
-<div>
-<Search  />
-</div> ) 
+    <div>
+    <Dropdown
+    options={options}
+    selected={selected}
+    setSelected={setSelected}
+    />   
+    </div> 
+ ) 
 });
---
+++ src/components/Dropdown.js
@@ -0,0 +1,56 @@
+import React,{useState, useEffect} from "react";
+
+const Dropdown = ({options, selected, setSelected}) => {
+
+    const [open, setOpen] = useState(false);
+
+    useEffect(()=>{
+        document.body.addEventListener('click',()=>{
+          setOpen(false)
+       }, {capture: true} )
+    },[]);
+    
+
+   const renderedOptions = options.map(option => {  
+
+        if(option.value === selected.value){
+
+        return null;
+
+        }
+
+
+    return (
+        <div 
+        key={option.value} 
+        className='item'
+        onClick={() => setSelected(option)}
+        >
+            {option.label}
+        </div>
+     );
+
+   })
+
+ 
+
+
+
+return (
+    <div className="ui form">
+        <div className="field">
+            <label className="label">select a color</label>
+            <div onClick = { ()=> setOpen(!open) }
+                 className={`ui selection dropdown ${ open ? 'visible active': '' }`}>
+                <i className="dropdown icon"></i>
+                <div className="text">{selected.label}</div>
+                <div className={`menu ${ open ? 'visible transition':'' }`}>{renderedOptions}
+                </div> 
+            </div>
+        </div>
+    </div>
+   );
+}
+
+
+export default Dropdown;
